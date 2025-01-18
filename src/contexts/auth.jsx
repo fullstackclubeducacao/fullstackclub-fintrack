ca92de1313bc2f58bc1403e63aff6300091ccb3b
Felipe Rocha <felipemota.rocha@gmail.com>
2025-01-18 20:05:06 -0300

    feat: add auth context to login page

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -24,6 +24,16 @@ export const AuthContextProvider = ({ children }) => {
       return response.data
     },
   })
+  const loginMutation = useMutation({
+    mutationKey: ['login'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users/login', {
+        email: variables.email,
+        password: variables.password,
+      })
+      return response.data
+    },
+  })
   useEffect(() => {
     const init = async () => {
       try {
@@ -61,12 +71,27 @@ export const AuthContextProvider = ({ children }) => {
       },
     })
   }
+  const login = (data) => {
+    loginMutation.mutate(data, {
+      onSuccess: (loggedUser) => {
+        const accessToken = loggedUser.tokens.accessToken
+        const refreshToken = loggedUser.tokens.refreshToken
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refreshToken)
+        setUser(loggedUser)
+        toast.success('Login realizado com sucesso!')
+      },
+      onError: (error) => {
+        console.error(error)
+      },
+    })
+  }
   return (
     <AuthContext.Provider
       value={{
-        user: user,
-        login: () => {},
-        signup: signup,
+        user,
+        login,
+        signup,
       }}
     >
       {children}
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
-import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -25,7 +23,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { api } from '@/lib/axios'
+import { AuthContext } from '@/contexts/auth'
 
 const loginSchema = z.object({
   email: z
@@ -43,17 +41,7 @@ const loginSchema = z.object({
 })
 
 const LoginPage = () => {
-  const [user, setUser] = useState(null)
-  const loginMutation = useMutation({
-    mutationKey: ['login'],
-    mutationFn: async (variables) => {
-      const response = await api.post('/users/login', {
-        email: variables.email,
-        password: variables.password,
-      })
-      return response.data
-    },
-  })
+  const { user, login } = useContext(AuthContext)
   const form = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -61,41 +49,9 @@ const LoginPage = () => {
       password: '',
     },
   })
-  useEffect(() => {
-    const init = async () => {
-      const accessToken = localStorage.getItem('accessToken')
-      const refreshToken = localStorage.getItem('refreshToken')
-      if (!accessToken && !refreshToken) return
-      try {
-        const response = await api.get('/users/me', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        setUser(response.data)
-      } catch (error) {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('refreshToken')
-        console.error(error)
-      }
-    }
-    init()
-  }, [])
-  const handleSubmit = (data) => {
-    loginMutation.mutate(data, {
-      onSuccess: (loggedUser) => {
-        const accessToken = loggedUser.tokens.accessToken
-        const refreshToken = loggedUser.tokens.refreshToken
-        localStorage.setItem('accessToken', accessToken)
-        localStorage.setItem('refreshToken', refreshToken)
-        setUser(loggedUser)
-        toast.success('Login realizado com sucesso!')
-      },
-      onError: (error) => {
-        console.error(error)
-      },
-    })
-  }
+
+  const handleSubmit = (data) => login(data)
+
   if (user) {
     return <h1>Olá, {user.first_name}!</h1>
   }
