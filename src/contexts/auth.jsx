875397abc5168d7c07c53349ac76cf6072046c4e
Felipe Rocha <felipemota.rocha@gmail.com>
2025-01-21 18:01:58 -0300

    feat: ensure access token is refreshed using refresh token on 401 error

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -66,7 +66,6 @@ export const AuthContextProvider = ({ children }) => {
         setUser(response.data)
       } catch (error) {
         setUser(null)
-        removeTokens()
         console.error(error)
       } finally {
         setIsInitializing(false)
