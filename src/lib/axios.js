7468bcf85669a358055aa8d7482bda4e9b68674c
Felipe Rocha <felipemota.rocha@gmail.com>
2025-01-21 17:36:22 -0300

    feat: ensure access token is attached on all api requests

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,17 @@
 import axios from 'axios'
 
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
+
 export const api = axios.create({
   baseURL: 'http://localhost:8080/api',
+})
+
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+  if (!accessToken) {
+    return request
+  }
+  console.log('Colocando accessToken na request')
+  request.headers.Authorization = `Bearer ${accessToken}`
+  return request
 })
