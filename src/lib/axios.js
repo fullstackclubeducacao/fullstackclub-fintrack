3e648da85c9e10d5d8d66fd7d8437538ec2b22c8
Felipe Rocha <felipemota.rocha@gmail.com>
2025-02-12 17:51:24 -0300

    feat: add header component

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,11 +6,11 @@ import {
 } from '@/constants/local-storage'
 
 export const protectedApi = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
 })
 
 export const publicApi = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
 })
 
 protectedApi.interceptors.request.use((request) => {
