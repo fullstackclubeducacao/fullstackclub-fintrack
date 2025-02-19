edfaf1a14b13fea232c8d68fe1000f6aef45a4cb
Felipe Rocha <felipemota.rocha@gmail.com>
2025-02-19 10:55:36 -0300

    feat: add get transactions hook

--- src/api/hooks/transaction.js
+++ src/api/hooks/transaction.js
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { useAuthContext } from '@/contexts/auth'
 
@@ -19,5 +19,20 @@ export const useCreateTransaction = () => {
         exact: false, // padrão
       })
     },
+  })
+}
+
+export const getTransactionsQueryKey = ({ userId, from, to }) => {
+  if (!from || !to) {
+    return ['getTransactions', userId]
+  }
+  return ['getTransactions', userId, from, to]
+}
+
+export const useGetTransactions = ({ from, to }) => {
+  const { user } = useAuthContext()
+  return useQuery({
+    queryKey: getTransactionsQueryKey({ userId: user.id, from, to }),
+    queryFn: () => TransactionService.getAll({ from, to }),
   })
 }
