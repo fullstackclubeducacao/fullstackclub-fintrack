b5abdb25568b48f3ae65d91bfef05cc339c598a7
Felipe Rocha <felipemota.rocha@gmail.com>
2025-03-10 08:48:59 -0300

    feat: add delete transaction hook

--- src/api/hooks/transaction.js
+++ src/api/hooks/transaction.js
@@ -59,3 +59,22 @@ export const useEditTransaction = () => {
     },
   })
 }
+
+export const deleteTransactionMutationKey = (id) => ['deleteTransaction', id]
+
+export const useDeleteTransaction = (id) => {
+  const queryClient = useQueryClient()
+  const { user } = useAuthContext()
+  return useMutation({
+    mutationKey: deleteTransactionMutationKey,
+    mutationFn: () => TransactionService.delete({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getUserBalanceQueryKey({ userId: user.id }),
+      })
+      queryClient.invalidateQueries({
+        queryKey: getTransactionsQueryKey({ userId: user.id }),
+      })
+    },
+  })
+}
